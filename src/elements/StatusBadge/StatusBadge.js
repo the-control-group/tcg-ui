e060ed11315597546ab8a2cdd9a5b2eded49c958
arnav-aggarwal <arnav.aggarwal@thecontrolgroup.com>
2019-05-14 11:50:12 -0700

    Get badges working

--- src/elements/StatusBadge/StatusBadge.js
+++ src/elements/StatusBadge/StatusBadge.js
@@ -8,54 +8,57 @@ import classNames from 'classnames';
 
 import Common from '../Common/Common';
 
-const StatusBadge = ({
-	children,
-	type = 'button',
-	loading,
-	disabled,
-	mini,
-	full,
-	outline,
-	bare,
-	small,
-	...other
-}) => {
-	const style = outline ? 'outline' : bare ? 'bare' : '';
+const StatusBadge = ({ children, active, paused, pending, archived, ...other }) => {
+	// const style = outline ? 'outline' : bare ? 'bare' : '';
 
 	const combinedClasses = classNames(
-		'ui-button',
+		'status-badge',
 		other.classes,
-		style,
-		small && 'small',
-		mini && 'mini',
-		full && 'full',
-		loading && 'loading'
+		active &&
+			'active',
+		paused &&
+			'paused',
+		pending &&
+			'pending',
+		archived &&
+			'archived'
+		// style,
 	);
 
 	return (
 		<Common
 			{...other}
-			classes={combinedClasses}
-			tag="button"
-			type={type}
-			disabled={disabled}
-		>
-			{children}
+			classes={
+				combinedClasses
+			}
+			tag="span"
+      >
+      <span className="dot" />
+			{active &&
+				'ACTIVE'}
+			{paused &&
+				'PAUSED'}
+			{pending &&
+				'PENDING'}
+			{archived &&
+				'ARCHIVED'}
 		</Common>
 	);
 };
 
 StatusBadge.propTypes = {
-	children: PropTypes.node.isRequired,
-	type: PropTypes.string, // DOM button `type` attribute
-	loading: PropTypes.bool, // Loading state
-	disabled: PropTypes.bool, // Disabled state
-	full: PropTypes.bool, // Full width size
-	mini: PropTypes.bool, // Mini size
-	outline: PropTypes.bool, // Outline style
-	bare: PropTypes.bool, // Bare style
-	small: PropTypes.bool, // Small button styles
-	secondary: PropTypes.bool // Secondary button styles
+	children:
+		PropTypes
+			.node
+			.isRequired
+	// loading: PropTypes.bool, // Loading state
+	// disabled: PropTypes.bool, // Disabled state
+	// full: PropTypes.bool, // Full width size
+	// mini: PropTypes.bool, // Mini size
+	// outline: PropTypes.bool, // Outline style
+	// bare: PropTypes.bool, // Bare style
+	// small: PropTypes.bool, // Small button styles
+	// secondary: PropTypes.bool // Secondary button styles
 };
 
 export default StatusBadge;
--- stories/StatusBadge.doc.js
+++ stories/StatusBadge.doc.js
@@ -2,9 +2,33 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { StatusBadge } from '../src';
 
-storiesOf('StatusBadge', module)
-	.add('Status Badge', () => (
-		<div>
-      <StatusBadge />
-		</div>
-	));
+storiesOf('StatusBadge', module).add('Status Badge', () => (
+	<div
+		style={{
+			'background-color':
+        '#121212',
+        padding: "2em",
+		}}
+	>
+		<StatusBadge
+			active
+		/>
+		<br />
+		<br />
+		<StatusBadge
+			paused
+		/>
+		<br />
+		<br />
+
+		<StatusBadge
+			pending
+		/>
+		<br />
+		<br />
+
+		<StatusBadge
+			archived
+		/>
+	</div>
+));
