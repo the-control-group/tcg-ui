40c450e8c4ed0cb9dec4e14412e72024c9f1708a
arnav-aggarwal <arnav.aggarwal@thecontrolgroup.com>
2019-05-15 10:50:53 -0700

    Fix linting issues

--- src/elements/StatusBadge/StatusBadge.js
+++ src/elements/StatusBadge/StatusBadge.js
@@ -3,36 +3,35 @@
  */
 
 import React from 'react';
-import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
 import Common from '../Common/Common';
 
 const StatusBadge = ({ status, ...other }) => {
 	const combinedClasses = classNames(
-    'status-badge',
-    status,
+		'status-badge',
+		status,
 		other.classes,
 	);
 
 	return (
 		<Common
-    classes={combinedClasses}
-    tag="span"
-    {...other}
-      >
-      <span className="status-badge-dot" />
+			classes={combinedClasses}
+			tag="span"
+			{...other}
+		>
+			<span className="status-badge-dot" />
 			{status.toUpperCase()}
 		</Common>
 	);
 };
 
-StatusBadge.PropTypes = {
-  status: (props, propName, componentName) => {
-    if (!['active', 'paused', 'pending', 'archived'].includes(props.status)){
-      return new Error(`Invalid prop: must have a prop 'status' with a value of one of ['active', 'paused', 'pending', 'status']`);
-    }
-  }
-}
+StatusBadge.propTypes = {
+	status: (props, propName, componentName) => {
+		if (!['active', 'paused', 'pending', 'archived'].includes(props.status)){
+			return new Error(`Invalid prop: ${componentName} must have a prop '${propName}' with a value of one of ['active', 'paused', 'pending', 'status']`);
+		}
+	}
+};
 
 export default StatusBadge;
