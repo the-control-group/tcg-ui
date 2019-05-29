e605e0cd876cc40f421d28ca441af4059406ef1e
arnav-aggarwal <arnav.aggarwal@thecontrolgroup.com>
2019-05-29 13:10:00 -0700

    Fix linter errors

--- src/elements/DataList/DataList.js
+++ src/elements/DataList/DataList.js
@@ -4,7 +4,6 @@
 
 import React, { Fragment } from 'react';
 import classNames from 'classnames';
-import PropTypes from 'prop-types';
 import Icon from '../Icon/Icon';
 
 import Common from '../Common/Common';
@@ -20,51 +19,71 @@ const DataList = ({ rows, label, ...other }) => {
 			classes={combinedClasses}
 			tag="div"
 			{...other}
-    >
-      <label className="label">{label}</label>
-      <hr />
+		>
+			<label className="label">{label}</label>
+			<hr />
 			{rows.map((data, index) => <DataListRow key={index} data={data} />)}
 		</Common>
 	);
 };
 
-const DataListRow = ({ data, ...other }) => {
-  return (
-    <Fragment>
-      <div className="data-list-item">
-        <span>{data.text}</span>
-        <Icon name="close-blue" height={14} width={30} onClick={data.xFn} />
-      </div>
-      <hr />
-    </Fragment>
-  );
-}
+const DataListRow = ({ data }) => {
+	return (
+		<Fragment>
+			<div className="data-list-item">
+				<span>{data.text}</span>
+				<Icon name="close-blue" height={14} width={30} onClick={data.xFn} />
+			</div>
+			<hr />
+		</Fragment>
+	);
+};
 
 DataList.propTypes = {
-  rows: (props, propName, componentName) => {
-    const err = new Error(`Invalid prop: ${componentName} must have a prop '${propName}' which must be a non-empty array of objects. The objects much have a "text" property of type string and a "xFn" property of type function`);
+	rows: (props, propName, componentName) => {
+		const err = new Error(`${componentName} must have a prop '${propName}' which must be a non-empty array of objects.
+The objects much have a "text" property of type string and a "xFn" property of type function`);
 
-    if(!Array.isArray(props.rows)) {
+		if(!Array.isArray(props.rows)) {
 			return err;
-    }
+		}
 
-    if(props.rows.length < 1) {
+		if(props.rows.length < 1) {
 			return err;
-    }
+		}
 
-    if (!props.rows.every(entry => typeof entry.text === 'string' && typeof entry.xFn === 'function')) {
+		if (!props.rows.every(entry => typeof entry.text === 'string' && typeof entry.xFn === 'function')) {
 			return err;
-    }
-  },
-  // onClick: (props, propName, componentName) => {
-	// 	if (!props.onClick) {
-	// 		return new Error(`Invalid prop: When given a prop 'action', ${componentName} must have a prop 'onClick'`);
-	// 	}
-    
-	// 	if (typeof props.onClick !== 'function') {
-	// 		return new Error(`Invalid prop: When given a prop 'action', ${componentName} must have a prop 'onClick' that is a function`);
-	// 	} 
-	// }
+		}
+	},
+	label: (props, propName, componentName) => {
+		if((typeof props.label !== 'string') || props.label.length < 1) {
+			return new Error(`${componentName} must have a prop '${propName}' which must be a non-empty string`);
+		}
+	}
+};
+
+DataListRow.propTypes = {
+	data: (props, propName, componentName) => {
+		const err = new Error(`${componentName} must have a prop '${propName}' which must be an object.
+The object must have a 'text' property which is a non-zero length string and a 'xFn' property which is a function`);
+
+		if(typeof props.data !== 'object') {
+			return err;
+		}
+
+		if(typeof props.data.text !== 'string') {
+			return err;
+		}
+
+		if(props.data.text.length < 1 ) {
+			return err;
+		}
+
+		if(typeof props.data.xFn !== 'function') {
+			return err;
+		}
+	}
 };
 
 export default DataList;
