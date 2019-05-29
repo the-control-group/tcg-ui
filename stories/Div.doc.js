240384ac54000bb70b2e86c3b8db85b4cf9635c9
Adam Rich <adamrich@Adams-MacBook-Pro.local>
2019-05-29 12:05:15 -0700

    adding layout components; adjusting variable structure

--- stories/Div.doc.js
+++ stories/Div.doc.js
@@ -5,15 +5,7 @@ import { Div } from '../src';
 
 storiesOf('Div', module)
 	.add('Div', () => (
-		<div>
-			<Div card>Card</Div>
-			<Div>
-				<Div subsection>Subsection</Div>
-			</Div>
-			<Div item>Item 1</Div>
-			<Div item>Item 2</Div>
-			<Div item>Item 3</Div>
-			<Div item>Item 4</Div>
-
-		</div>
+		<Div>
+			div
+		</Div>
 	));
