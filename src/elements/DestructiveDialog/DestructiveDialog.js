5c7139e7441223ce1ea37365c727514dc8ed3541
Lauren Settle <lauren.settle@thecontrolgroup.com>
2019-07-10 11:26:36 -0700

    fixes destructive dialog styling and implements modal-style overlay/positioning

--- src/elements/DestructiveDialog/DestructiveDialog.js
+++ src/elements/DestructiveDialog/DestructiveDialog.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import Button from '../Button/Button';
 
 const DestructiveDialog = ({
 	title,
 	content,
-	height = 128,
-	width = 280,
+	height = 144,
+	width = 296,
 	primaryActionClickHandler,
 	primaryActionContent,
 	secondaryActionClickHandler,
@@ -14,21 +14,24 @@ const DestructiveDialog = ({
 }) => {
 	//TODO: Switch out h1/p elements to utilize Text component when ready
 	return (
-		<div
-			className="ui-destructive-dialog-container"
-			style={{ height, width }}
-		>
-			<h1 className="destructive-dialog-title">{title}</h1>
-			<p className="destructive-dialog-content">{content}</p>
-			<div className="destructive-dialog-button-wrapper">
-				<Button onClick={primaryActionClickHandler} bare>
-					{primaryActionContent}
-				</Button>
-				<Button onClick={secondaryActionClickHandler} bare>
-					{secondaryActionContent}
-				</Button>
+		<Fragment>
+			<div className="ui-modal-overlay" />
+			<div
+				className="ui-destructive-dialog-container"
+				style={{ height, width }}
+			>
+				<h1 className="destructive-dialog-title">{title}</h1>
+				<p className="destructive-dialog-content">{content}</p>
+				<div className="destructive-dialog-button-wrapper">
+					<Button onClick={primaryActionClickHandler} bare>
+						{primaryActionContent}
+					</Button>
+					<Button onClick={secondaryActionClickHandler} bare>
+						{secondaryActionContent}
+					</Button>
+				</div>
 			</div>
-		</div>
+		</Fragment>
 	);
 };
 
@@ -37,6 +40,7 @@ DestructiveDialog.propTypes = {
 	content: PropTypes.string.isRequired,
 	height: PropTypes.number,
 	width: PropTypes.number,
+	dismiss: PropTypes.func.isRequired,
 	primaryActionClickHandler: PropTypes.func.isRequired,
 	primaryActionContent: PropTypes.string.isRequired,
 	secondaryActionClickHandler: PropTypes.func.isRequired,
